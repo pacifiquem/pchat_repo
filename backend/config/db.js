ac6b4dd73278e8a4755c13cc19d85a411acb51be
pacy001 <pacifiquemurangwa001@gmail.com>
2022-05-03 12:08:05 +0200

    db configuration modified

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,20 @@ const dotenv = require('dotenv').config({path: './config.env'});
 const colors = require('colors');
 
 const dbConnection =async () => {
-   await mongoose.connect(process.env.dbUrl,{
-        useNewUrlParser : true,
-        useUnifiedTopology: true
-    }, () => {
-        console.log(`database is connected`.bgBlack.cyan.bold);
-    });
+
+    try {
+        const conn =  await mongoose.connect(process.env.dbUrl,{
+            useNewUrlParser : true,
+            useUnifiedTopology: true
+        })
+
+        if(conn) {
+            console.log(`database is connected`.bgBlack.cyan.bold);
+        }
+    } catch (error) {
+        console.log(error);
+        mongoose.connection.close()
+    }
 }
 
 module.exports = dbConnection;
