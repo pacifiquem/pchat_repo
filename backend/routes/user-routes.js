3c479ef12613112d9705184586ef6671d9e7ceee
pacy001 <pacifiquemurangwa001@gmail.com>
2022-05-03 12:09:32 +0200

    user-routes modified

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -3,8 +3,11 @@ const dotenv = require('dotenv').config({path: '../config/config.env'});
 const user_controllers = require('../controllers/user-controller');
 const router = express.Router();
 
+const {signUp} = require('../controllers/user-controller');
 
-
+router
+    .route('/signup')
+    .post(signUp);
 
 
 module.exports = router;
